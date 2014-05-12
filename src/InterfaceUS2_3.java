35938cbcc81fd94d30d0c012696697b4f29dcc5b
Anne-So <Anne-So@172.16.126.131>
2014-05-12 14:59:51 +0200

    mise à jour interface US2

--- src/InterfaceUS2_3.java
+++ src/InterfaceUS2_3.java
@@ -21,11 +21,26 @@ public class InterfaceUS2_3 extends JFrame implements ActionListener
 	private JPanel panelClient;
 	
 	private JLabel labCategorie;
-	private JTextField categorie;
+	private JComboBox categorie;
 	private JLabel labSp;
-	private JTextField espece;
-	private JLabel labSp;
-	private JTextField espece;
+	private JComboBox espece;
+	private JLabel labNomAn;
+	private JTextField nomAnimal;
+	private JLabel labSexe;
+	private JComboBox sexe;
+	private JLabel labDateNaissance;
+	private JTextField dateNaissance;
+	private JPanel panelAnimal;
+	private JPanel panelAni1;
+	private JPanel panelAni2;
+	private JPanel panelAni3;
+	private JPanel panelAni4;
+	private JPanel panelAni5;
+	
+	private JButton butAnnuler;
+	private JButton butValider;
+	private JPanel panButton;
+	private JPanel panelButton;
 	
 	public InterfaceUS2_3()
 	{
@@ -37,6 +52,64 @@ public class InterfaceUS2_3 extends JFrame implements ActionListener
 		
 		panelClient.setBorder(BorderFactory.createTitledBorder(
 	            BorderFactory.createEtchedBorder(), "Client :"));
+		
+		labCategorie = new JLabel("Catégorie de l'animal");
+		categorie = new JComboBox();
+		JPanel panelAni1 = new JPanel();
+		panelAni1.setLayout(new GridLayout(1,2));
+		panelAni1.add(labCategorie);
+		panelAni1.add(categorie);
+		
+		labSp = new JLabel("Espèce de l'animal");
+		espece = new JComboBox();
+		JPanel panelAni2 = new JPanel();
+		panelAni2.setLayout(new GridLayout(1,2));
+		panelAni2.add(labSp);
+		panelAni2.add(espece);
+		
+		labNomAn = new JLabel("Nom de l'animal");
+		nomAnimal = new JTextField();
+		JPanel panelAni3 = new JPanel();
+		panelAni3.setLayout(new GridLayout(1,2));
+		panelAni3.add(labNomAn);
+		panelAni3.add(nomAnimal);
+		
+		labSexe = new JLabel("Sexe de l'animal");
+		sexe = new JComboBox();
+		JPanel panelAni4 = new JPanel();
+		panelAni4.setLayout(new GridLayout(1,2));
+		panelAni4.add(labSexe);
+		panelAni4.add(sexe);
+		
+		labDateNaissance = new JLabel("Date de naissance de l'animal");
+		dateNaissance = new JTextField();
+		JPanel panelAni5 = new JPanel();
+		panelAni5.setLayout(new GridLayout(1,2));
+		panelAni5.add(labDateNaissance);
+		panelAni5.add(dateNaissance);
+		
+		panelAnimal = new JPanel();
+		panelAnimal.setLayout(new GridLayout(5, 1));
+		panelAnimal.add(panelAni1);
+		panelAnimal.add(panelAni2);
+		panelAnimal.add(panelAni3);
+		panelAnimal.add(panelAni4);
+		panelAnimal.add(panelAni5);
+		
+		this.add(panelAnimal, BorderLayout.CENTER);
+		
+		panelAnimal.setBorder(BorderFactory.createTitledBorder(
+	            BorderFactory.createEtchedBorder(), "Nouvel animal :"));
+		
+		butAnnuler = new JButton("Annuler");
+	    butValider = new JButton("Valider");
+	    panButton = new JPanel();
+	    panButton.add(butAnnuler);
+	    panButton.add(butValider);
+	    panelButton = new JPanel();
+	    panelButton.add(panButton); 
+	    
+	    this.add(panelButton, BorderLayout.SOUTH);
 		
 		
 		 // visibilité

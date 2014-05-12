d40cf7f4ffef6a07dcf96126e76e76f755a3268e
Altarrys <Altarrys@172.16.126.156>
2014-05-12 11:51:47 +0200

    a

--- src/Main.java
+++ src/Main.java
@@ -15,13 +15,12 @@ public class Main {
 		
 		//s.printSample(s);
 
-		Customers c = new Customers(null, 0, null, null, 0);
-		c.setName("Auguste", "Dupond");
-		c.setAdressClient(new Adress(56,"rue des Pivers",59000,"Dunkerque"));
+		Customers c = new Customers(null, 0, null, null, 1);
+		c.setName("Aria", "Durand");
+		c.setAdressClient(new Adress(11,"av du Cmdt Gabon",15200,"Mauriac"));
+		c.setNomSociete("Elevage des Babines");
 		c.setPro(false);
 		
-
-		System.out.println(d.IfCustomerExist(new Customers(null, 0, null, null, 1)));
 		
 		System.out.println(d.IfCustomerExist(c));
 

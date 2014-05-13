547a620c6d2aed5f17ca57d8ea9e0cc346221a80
ameliesimon <Amlie@PC-Mili.wifi.univ-poitiers.fr>
2014-05-13 14:32:34 +0200

    Main

--- src/Main.java
+++ src/Main.java
@@ -7,38 +7,18 @@ public class Main {
 	{
 		Database d = new Database();
 		
-		
-		
-		//d.DisplayResultsQuery("Select IDSAMPLE, NAMETYPE from SAMPLE where IDSAMPLE=2;");
-		
-		//Samples s = d.searchSample(""+2);
-		
-		//s.printSample(s);
-
 		Customers c = new Customers(null, 0, null, null, 0);
-		c.setName("Lili", "Jones");
-		c.setPhone("+33148963214");
-		//c.setNomSociete("Equita");
-		//c.setDepartementFacturation("Compta");
-		//c.setEmail("lTaylor@equita.com");
-		c.setAdressClient(new Adress(63,"avenue Perrier",31000,"Toulouse"));		
-		c.getAdressClient().setIdAdress(22);
-		//c.setAdressFacturation(c.getAdressClient());
-		//c.getAdressFacturation().setIdAdress(22);				
+		c.setName("jeje", "Peyro");
+		c.setPhone("+33148963256");
+		c.setAdressClient(new Adress(25,"rue Apero",26000,"Valence"));
 		c.setPro(false);
 		
-
-		d.saveCustomer(c);
-
-
-//		ArrayList<Types_analysis> t =  d.searchTypesAnalysis("plume", "Sexage");
-//		
-//		for (int i = 0; i < t.size(); i++)
-//		{
-//			System.out.println(i);
-//		}
-//		
+		//System.out.println(d.IfCustomerExist(c));
 		
+		boolean pro = d.saveCustomer(c);
+		
+		System.out.println(pro);
+
 		d.close();
 
 
